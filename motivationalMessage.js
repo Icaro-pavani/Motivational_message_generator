4b8a3661841868a5782954f1314dacef6883c6da
Icaro-pavani <icaro.pavani79@gmail.com>
2022-01-05 18:39:23 -0300

    Add the components to form the motivational message

---
+++ motivationalMessage.js
@@ -0,0 +1,6 @@
+// First phrase of the motivational message
+const firstPart = ['Love for all', 'Change the world', 'Nerver regret anything', 'Aspire to inspire'];
+// Connectors to the phrases
+const connector = [', ', ' by ', ' that ', ' before '];
+// Second phrase of the message
+const secondPart = ['hatred for none.', 'being yourself.', 'made you smile.', 'we expire.'];
